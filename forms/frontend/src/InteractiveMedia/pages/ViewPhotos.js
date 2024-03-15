601613fe8d33f4220a0e9e92039f5a26c1f9a236
Ben Ockert <55101856+benockert@users.noreply.github.com>
2024-03-14 22:51:21 -0700

    First attempt at pagination via scrolling

--- forms/frontend/src/InteractiveMedia/pages/ViewPhotos.js
+++ forms/frontend/src/InteractiveMedia/pages/ViewPhotos.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import { getData } from "../../api";
-import { useLoaderData, redirect } from "react-router-dom";
+import { useLoaderData, useParams, redirect } from "react-router-dom";
 
 export async function viewPhotosPageLoader({ params, request }) {
   const eventId = params.eventId;
-  return await getData(`media/${eventId}/photo_mosaic`).then((res) => {
-    if (res.statusCode === 404) {
-      return redirect("/");
-    } else {
-      return res.data ?? {};
+  return await getData(`media/${eventId}/photo_mosaic?pageSize=${10}`).then(
+    (res) => {
+      if (res.statusCode === 404) {
+        return redirect("/");
+      } else {
+        return res.data ?? {};
+      }
     }
-  });
+  );
 }
 
 const ImageGalleryList = styled("ul")(({ theme }) => ({
@@ -36,18 +38,64 @@ const ImageGalleryList = styled("ul")(({ theme }) => ({
 }));
 
 const ViewPhotos = () => {
-  const { items } = useLoaderData();
+  const { eventId } = useParams();
+  const { items, lek } = useLoaderData();
+  const [nextKey, setNextkey] = useState(lek);
+  const [images, setImages] = useState(items);
+  console.log("Starting:", { images, nextKey });
+
+  const getNextPage = async () => {
+    await getData(
+      `media/${eventId}/photo_mosaic?lek=${nextKey}&pageSize=${5}`
+    ).then((res) => {
+      const { items, lek } = res.data;
+      setNextkey(lek);
+      setImages([items, ...images]); // keep existing and perserve order
+      console.log("New:", images);
+    });
+  };
+
+  // event listending handler for reaching the bottom of the page
+  // request the next page of data
+  const handleScroll = (event) => {
+    if (
+      Math.abs(
+        event.target.scrollingElement.scrollHeight -
+          (event.target.scrollingElement.scrollTop +
+            event.target.scrollingElement.clientHeight)
+      ) <= 1
+    ) {
+      getNextPage();
+    }
+  };
+
+  useEffect(() => {
+    // only add an event listener if there is more data to retrieve
+    if (nextKey) {
+      console.log("Mounting");
+      window.addEventListener("scroll", (event) => handleScroll(event));
+      return () => {
+        console.log("Unmounting");
+        window.removeEventListener(
+          "scroll",
+          console.log("Removing scroll listener")
+        );
+      };
+    }
+  }, [nextKey]); // only cleanup/add another listening if the key has changed, otherwise we will get the same data
+
   return (
     <Box sx={{ backgroundColor: "#171717", minHeight: "100vh" }}>
       <Stack direction="row" justifyContent="center" alignItems="center">
         <ImageGalleryList>
-          {items.map((image, index) => (
+          {images.map((image, index) => (
             <ImageListItem key={index} sx={{ border: "solid white 1px" }}>
               <img
                 srcSet={`${image.thumbnail_image} 100w, ${image.full_image} 400w`}
                 sizes="(max-width: 600px) 100px, 400px"
                 src={image.full_image}
                 loading="lazy"
+                alt=""
               />
               <ImageListItemBar
                 title={`Message: ${image.message}`}
